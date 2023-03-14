110edaf245e43ec62ed00a1cca21babf213efd37
Klebert-01 <arvid.berkert@gmail.com>
2023-03-14 19:41:08 +0100

    added connectionstring to sql-db

--- KlebertsWebshop/Server/Program.cs
+++ KlebertsWebshop/Server/Program.cs
@@ -9,10 +9,15 @@ namespace KlebertsWebshop
 
 			// Add services to the container.
 
+			builder.Services.AddEndpointsApiExplorer(); // jag lade till
+			builder.Services.AddSwaggerGen();
+
 			builder.Services.AddControllersWithViews();
 			builder.Services.AddRazorPages();
 
 			var app = builder.Build();
+
+			app.UseSwaggerUI();
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
@@ -25,6 +30,8 @@ namespace KlebertsWebshop
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+
+			app.UseSwagger();
 
 			app.UseHttpsRedirection();
 
