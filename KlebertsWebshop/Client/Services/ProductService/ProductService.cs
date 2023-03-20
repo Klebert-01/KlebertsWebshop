435b86de70840e959aa622bab2e438a092e7c55f
Klebert-01 <arvid.berkert@gmail.com>
2023-03-20 14:17:30 +0100

    add searchbar

--- KlebertsWebshop/Client/Services/ProductService/ProductService.cs
+++ KlebertsWebshop/Client/Services/ProductService/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using KlebertsWebshop.Shared;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Json;
 
 namespace KlebertsWebshop.Client.Services.ProductService
@@ -13,6 +14,7 @@ namespace KlebertsWebshop.Client.Services.ProductService
         }
 
         public List<Product> Products { get; set; } = new();
+        public string Message { get; set; } = "Loading products...";
 
         public event Action ProductsChanged;
 
@@ -34,6 +36,24 @@ namespace KlebertsWebshop.Client.Services.ProductService
             ProductsChanged.Invoke();
         }
 
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<String>>>($"/api/product/searchsuggestions/{searchText}");
 
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<Product>>>($"/api/product/search/{searchText}");
+
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 9)
+                Message = "No products found";
+            ProductsChanged?.Invoke();
+        }
     }
 }
--- KlebertsWebshop/Server/Services/ProductService/ProductService.cs
+++ KlebertsWebshop/Server/Services/ProductService/ProductService.cs
@@ -1,54 +1,105 @@
-﻿namespace KlebertsWebshop.Server.Services.ProductService
+﻿namespace KlebertsWebshop.Server.Services.ProductService;
+
+public class ProductService : IProductService
 {
-    public class ProductService : IProductService
+    private readonly DataContext _context;
+
+    public ProductService(DataContext context)
     {
-        private readonly DataContext _context;
+        _context = context;
+    }
 
-        public ProductService(DataContext context)
+    public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+    {
+        var response = new ServiceResponse<List<Product>>
         {
-            _context = context;
+            Data = await _context.Products.Include(p => p.Variants).ToListAsync()
+        };
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+    {
+        var response = new ServiceResponse<Product>();
+        var product = await _context.Products
+            .Include(p => p.Variants)
+            .ThenInclude(p => p.ProductType)
+            .FirstOrDefaultAsync(p => p.Id == productId);
+
+        if (product == null)
+        {
+            response.Success = false;
+            response.Message = "Sorry this product is not available.";
         }
-
-        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        else
         {
-            var response = new ServiceResponse<List<Product>>
-            {
-                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
-            };
-
-            return response;
+            response.Data = product;
         }
+        return response;
+    }
 
-        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+    public async Task<ServiceResponse<List<Product>>> GetProductsByCategoryAsync(string categoryUrl)
+    {
+        var response = new ServiceResponse<List<Product>>
         {
-            var response = new ServiceResponse<Product>();
-            var product = await _context.Products
+            Data = await _context.Products
+                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
                 .Include(p => p.Variants)
-                .ThenInclude(p => p.ProductType)
-                .FirstOrDefaultAsync(p => p.Id == productId);
+                .ToListAsync()
+        };
+        return response;
+    }
+    public async Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText)
+    {
+        var products = await FindProductsBySearchText(searchText);
 
-            if (product == null)
-            {
-                response.Success = false;
-                response.Message = "Sorry this product is not available.";
-            }
-            else
-            {
-                response.Data = product;
-            }
-            return response;
-        }
+        List<string> results = new();
 
-        public async Task<ServiceResponse<List<Product>>> GetProductsByCategoryAsync(string categoryUrl)
+        foreach (var product in products)
         {
-            var response = new ServiceResponse<List<Product>>
+            if (product.Title.Contains(searchText, StringComparison.OrdinalIgnoreCase))
             {
-                Data = await _context.Products
-                    .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
-                    .Include(p => p.Variants)
-                    .ToListAsync()
-            };
-            return response;
+                results.Add(product.Title);
+            }
+
+            if(product.Description != null)
+            {
+                var punctuation = product.Description.Where(char.IsPunctuation)
+                    .Distinct().ToArray();
+                var words = product.Description.Split()
+                    .Select(s => s.Trim(punctuation));
+
+                foreach (var word in words)
+                {
+                    if(word.Contains(searchText, StringComparison.OrdinalIgnoreCase)
+                        && !results.Contains(word))
+                    {
+                        results.Add(word);
+                    }
+                }
+            }
         }
+
+        return new ServiceResponse<List<string>> { Data = results };
+    }
+
+    public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+    {
+        var response = new ServiceResponse<List<Product>>
+        {
+            Data = await FindProductsBySearchText(searchText)
+        };
+        return response;
+    }
+
+    private async Task<List<Product>> FindProductsBySearchText(string searchText)
+    {
+        return await _context.Products
+                        .Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+                        ||
+                        p.Description.ToLower().Contains(searchText.ToLower()))
+                        .Include(p => p.Variants)
+                        .ToListAsync();
     }
 }
